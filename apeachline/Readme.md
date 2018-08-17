b4fcb0137b86edac0073ba8b3e8ce093f720058c
Alex Poddubnyi <poddubnyiav@gmail.com>
2018-08-17 14:51:17 +0300

    Add a Maven project that run a test for eachLime() method

---
+++ apeachline/Readme.md
@@ -0,0 +1,7 @@
+#Task 
+Create a Spock specification to test eachLine() method that iterates through multiline File or String 
+
+# Steps to run the test
+1. Import as Maven project 
+2. Navigate to \src\test\groovy\apeachline
+3. Run 'EachLineKindaUnitTestSpec.groovy' as a JUnit test 
---
+++ apeachline/src/test/groovy/apeachline/EachLineKindaUnitTestSpec.groovy
@@ -0,0 +1,81 @@
+package apeachline
+
+import spock.lang.*
+
+/**
+ * @author  Alex Poddubnyi
+ * Below test cases are valid for File object as well
+ */
+
+class EachLineKindaUnitTestSpec extends Specification {
+	
+	def "Verify if eachLine method iterates through each line"(){
+		
+		given:
+			def multiLineString = "\nline2\nline3"
+//			def multiLineString = new File("src/test/resources/multilineFile.txt")
+			def actualResultsList = []
+			
+		when: "Iterate through the given String"
+			multiLineString.eachLine{actualResultsList << it}
+							
+		then: "Verify each element of the actualResultsList"
+			actualResultsList == ["", "line2", "line3"]
+	}	
+	
+	def "Check if it returns the last value returned by closure"(){
+		
+		given:
+			def multiLineString = "\nThis is simple multiline string,\nthat I chose for simple test\nand this is a last line"
+			
+		when: "Iterate through the given String"
+			def result = multiLineString.eachLine{it}
+							
+		then: "Verify the last value returned by the closure"
+			result == "and this is a last line"
+	}
+			
+	def "Test firstLine parameter default"(){
+		
+		given:
+			def actualString
+			def defaultFirstLine = 0 // JavaDoc says '(default is 1, set to 0 to start counting from 0)' but it actually set to 0 by default
+									//And there was an issue https://issues.apache.org/jira/browse/GROOVY-2905 
+			def multiLineString = """This is simple multiline string,
+										that I chose as a test data
+										and add couple more lines
+										to check firstLine parameter"""
+			
+		when: "Iterate through the given String"
+			def result = multiLineString.eachLine{line, number ->
+				if (number == defaultFirstLine){
+					actualString = line
+				}
+			}
+							
+		then: "Verify first line "
+			actualString == "This is simple multiline string,"
+	}
+	
+	def "Test firstLine parameter custom"(){
+		
+		given:
+			def actualString
+			def firstLineNumber = 2
+			def multiLineString = """This is simple multiline string,
+										that I chose as a test data
+										and add couple more lines
+										to check firstLine parameter"""
+			
+		when: "Iterate through the given String"
+			def result = multiLineString.eachLine(firstLineNumber){line, number ->
+				if (number == firstLineNumber){
+					actualString = line
+				}
+			}
+							
+		then: "Verify first line "
+			actualString == "This is simple multiline string,"
+	}
+
+}
